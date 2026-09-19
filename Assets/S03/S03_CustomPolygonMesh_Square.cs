aa480c29dae6303c4a286ff9540f93b3c5cc2edd
zziminally <zziminally@gmail.com>
2026-09-19 19:16:22 +0900

    scene: fill out vertices

--- Assets/S03/S03_CustomPolygonMesh_Square.cs
+++ Assets/S03/S03_CustomPolygonMesh_Square.cs
@@ -9,17 +9,21 @@
           Vector3[] vertices = new Vector3[]
           {
               // 예: new Vector3(0f, 1f, 0f),
-            new Vector3(0f, 1f, 0f), // 0
-            new Vector3(1f, 1f, 0f), // 1
-            new Vector3(1f, 0f, 0f), // 2
-            new Vector3(0f, 0f, 0f), // 3
+            new Vector3(1f, 0f, 0f), // 0
+            new Vector3(2f, 0f, 0f), // 1
+            new Vector3(3f, 1f, 0f), // 2
+            new Vector3(3f, 2f, 0f), // 3
+            new Vector3(2f, 3f, 0f), // 4
+            new Vector3(1f, 3f, 0f), // 5
+            new Vector3(0f, 2f, 0f), // 6
+            new Vector3(0f, 1f, 0f), // 7
           };
 
           // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
           int[] triangles = new int[6]
           {
-              0, 1, 2,
-              0, 2, 3
+            0, 1, 2,
+            0, 2, 3
           };
 
           Mesh mesh = new Mesh();
